943112b32cb332056e8103cc09ca4eb79e2d65bb
Adrian Bermea <abermea@outlook.com>
2021-01-05 13:53:30 -0600

    cleanup

--- arrays/minimum-swaps-2/main.py
+++ arrays/minimum-swaps-2/main.py
@@ -10,25 +10,6 @@ def minimumSwaps(arr):
             swaps += 1
     return swaps
 
-def minimumSwapsO(arr):
-    swaps = 0
-    lenarr = len(arr)
-    for i in range(0, lenarr):
-        if(arr[i] != (i + 1)):
-            index = arr[i:lenarr].index(i + 1)
-            arr[i], arr[index + i], swaps = arr[index + i], arr[i], swaps + 1
-    
-    return swaps
-
-def diff_arrays(a, b):
-    return [abs(x - y) for x,y in zip(a,b)]
-
-def first(arr):
-    sorted_arr = sorted(arr)
-    diff = diff_arrays(arr, sorted_arr)
-    filtered = list(filter(lambda x: x != 0, diff))
-    return len(filtered) - 1
-
 # Test cases
 
 arr = [7, 1, 3, 2, 4, 5, 6]
